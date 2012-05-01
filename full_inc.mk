628cfebcf68ded36416aecd489bc744feed2865a
Scott Busche <swbusche@gmail.com>
2012-04-30 20:03:45 -0500

    Inc: Add HWA and bugfixes (1/2)
    Merges in the latest fixes/changes from Team Evervolv.
    
    Changes include:
    	New Tiamat Kernel
    	Working HWA
    	Fixes for USB plug/unplug restarts
    	/emmc appears in storage menu, but doesn't mount when connected by USB
    
    Signed-off-by: Scott Busche <swbusche@gmail.com>
    Change-Id: I093fdb68b57d8d6f21bf222ac21d7531b46a79e1

--- full_inc.mk
+++ full_inc.mk
@@ -14,10 +14,13 @@
 # limitations under the License.
 #
 
-#
-# This is the product configuration for a generic incredible,
-# not specialized for any geography.
-#
+# Overlay / Locale
+DEVICE_PACKAGE_OVERLAYS := device/htc/inc/overlay
+PRODUCT_LOCALES := en
+
+# Inc uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal hdpi
+PRODUCT_AAPT_PREF_CONFIG := hdpi
 
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us.mk)
@@ -27,32 +30,35 @@ PRODUCT_COPY_FILES += \
     device/htc/inc/prebuilt/root/init.inc.usb.rc:root/init.inc.usb.rc \
     device/htc/inc/prebuilt/root/ueventd.inc.rc:root/ueventd.inc.rc
 
-$(call inherit-product-if-exists, vendor/htc/inc/inc-vendor.mk)
-
 PRODUCT_PROPERTY_OVERRIDES += \
-	ro.com.android.wifi-watchlist=GoogleGuest \
 	ro.error.receiver.system.apps=com.google.android.feedback \
 	ro.setupwizard.enterprise_mode=1 \
 	ro.com.google.clientidbase=android-verizon \
 	ro.com.google.locationfeatures=1 \
-	ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
-	ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
-	ro.cdma.home.operator.numeric=310004 \
 	ro.cdma.home.operator.alpha=Verizon \
+	ro.cdma.home.operator.numeric=310012 \
 	ro.cdma.homesystem=64,65,76,77,78,79,80,81,82,83 \
 	ro.cdma.data_retry_config=default_randomization=2000,0,0,120000,180000,540000,960000 \
 	ro.config.vc_call_vol_steps=7 \
 	ro.cdma.otaspnumschema=SELC,1,80,99 \
 	ro.telephony.call_ring.multiple=false \
 	ro.telephony.call_ring.delay=3000 \
-	ro.url.safetylegal=http://www.htc.com/staticfiles/Support/legal/?model=A855 \
 	ro.setupwizard.enable_bypass=1 \
-	dalvik.vm.lockprof.threshold=500 \
-	dalvik.vm.dexopt-flags=m=y \
 	ro.media.dec.jpeg.memcap=20000000 \
 	ro.media.enc.jpeg.quality=95,85,70
 
-DEVICE_PACKAGE_OVERLAYS += device/htc/inc/overlay
+# Dalvik properties
+# dexop-flags: "v=" n|r|a, "o=" n|v|a|f, "m=y" register map
+# v=verify o=optimize: n=none r=remote a=all f=full v=verified
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.dexopt-flags=m=y \
+    dalvik.vm.checkjni=false
+
+# Default heap settings for 512mb device
+include frameworks/base/build/phone-hdpi-512-dalvik-heap.mk
+
+# we have enough storage space to hold precise GC data
+PRODUCT_TAGS += dalvik.gc.type-precise
 
 PRODUCT_COPY_FILES += \
     frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
@@ -82,6 +88,14 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     device/htc/inc/prebuilt/usr/keychars/incrediblec-keypad.kcm.bin:system/usr/keychars/incrediblec-keypad.kcm.bin
 
+# sysctl parameters
+PRODUCT_COPY_FILES += \
+    device/htc/inc/prebuilt/etc/sysctl.conf:system/etc/sysctl.conf
+
+# non-rotational init.d script
+PRODUCT_COPY_FILES += \
+    device/htc/inc/prebuilt/etc/init.d/02nonrot:system/etc/init.d/02nonrot
+
 # media config xml file
 PRODUCT_COPY_FILES += \
     device/htc/inc/prebuilt/etc/media_profiles.xml:system/etc/media_profiles.xml
@@ -106,40 +120,66 @@ PRODUCT_COPY_FILES += \
 
 # Kernel modules
 PRODUCT_COPY_FILES += \
-    device/htc/inc/prebuilt/lib/modules/bcm4329.ko:system/lib/modules/bcm4329.ko
+    device/htc/inc/prebuilt/lib/modules/bcm4329.ko:system/lib/modules/bcm4329.ko \
+    device/htc/inc/prebuilt/lib/modules/ifb.ko:system/lib/modules/ifb.ko
 
-# HW drivers and libs
+#
+# Packages needed for Inc
+#
+# Sensors
 PRODUCT_PACKAGES += \
-    librs_jni \
-    sensors.inc \
-    lights.inc \
+    com.android.future.usb.accessory \
     gps.inc \
-    camera.qsd8k \
+    lights.inc \
+    sensors.inc \
+    librs_jni \
+    camera.qsd8k
+# Audio
+PRODUCT_PACKAGES += \
     audio.a2dp.default \
     audio.primary.qsd8k \
-    audio_policy.qsd8k \
+    audio_policy.qsd8k
+# GPU
+PRODUCT_PACKAGES += \
+    copybit.qsd8k \
     gralloc.qsd8k \
-    com.android.future.usb.accessory
-#    libOmxCore \
-#    libOmxVidEnc \
-#    copybit.qsd8k \
+    hwcomposer.qsd8k \
+    libgenlock \
+    libmemalloc \
+    libtilerenderer \
+    libQcomUI
+# OMX
+PRODUCT_PACKAGES += \
+    libOmxCore \
+    libOmxVidEnc \
+    libOmxVdec \
+    libstagefrighthw
 
-# Disable HWAccel for now
+# Enable GPU composition (0: cpu, 1: gpu)
+# Note: must be 1 for composition.type to work
+PRODUCT_PROPERTY_OVERRIDES += debug.sf.hw=1
+
+# Enable copybit composition
+PRODUCT_PROPERTY_OVERRIDES += debug.composition.type=mdp
+
+# Force 2 buffers since gralloc defaults to 3 (we only have 2)
+PRODUCT_PROPERTY_OVERRIDES += debug.gr.numframebuffers=2
+
+# HardwareRenderer properties
+# dirty regions: "false" disables partial invalidates (override if enabletr=true)
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.disable_hw_accel=true
+    hwui.render_dirty_regions=false \
+    hwui.disable_vsync=true \
+    hwui.print_config=choice \
+    debug.enabletr=false
 
 # USB
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    persist.sys.usb.config=mass_storage
+ADDITIONAL_DEFAULT_PROPERTIES += \
+    persist.sys.usb.config=mass_storage \
+    persist.service.adb.enable=1
 
-# we have enough storage space to hold precise GC data
-PRODUCT_TAGS += dalvik.gc.type-precise
-
-PRODUCT_LOCALES += en
-
-# Inc uses high-density artwork where available
-PRODUCT_AAPT_CONFIG := normal hdpi
-PRODUCT_AAPT_PREF_CONFIG := hdpi
+# Set dirty_ratio for UMS
+PRODUCT_PROPERTY_OVERRIDES += ro.vold.umsdirtyratio=20
 
 ifeq ($(TARGET_PREBUILT_KERNEL),)
     LOCAL_KERNEL := device/htc/inc/prebuilt/root/kernel
